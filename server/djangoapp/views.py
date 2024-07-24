58aa5d5e2c67c74c73615fc2088d65578f90c1d0
Mohamed Ghdour <91890072+mohamedghdour@users.noreply.github.com>
2024-07-24 08:59:00 +0100

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -109,6 +109,10 @@ def add_review(request):
             post_review(data)
             return JsonResponse({"status": 200})
         except SomeSpecificException as e:
-            return JsonResponse({"status": 401, "message": "Error in posting review"})
+            return JsonResponse(
+                {"status": 401,
+                 "message": "Error in posting review"
+                }
+            )
     else:
         return JsonResponse({"status": 403, "message": "Unauthorized"+e})
