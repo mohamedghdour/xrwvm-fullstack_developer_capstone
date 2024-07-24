4cac1d64b0f50009bd537bfb5d5c11a1956ea001
ghdour <ghdourmohamed@gmail.com>
2024-07-23 23:14:06 -0400

    user management

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -21,6 +21,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('login/', TemplateView.as_view(template_name="index.html")),
+        path('register/', TemplateView.as_view(template_name="index.html")),
     path('contact/', TemplateView.as_view(template_name="Contact.html")),
     path('about/', TemplateView.as_view(template_name="About.html")),
     path('admin/', admin.site.urls),
